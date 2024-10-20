23d519be7eef39beba771b5af61edcb4288d9909
Yasser Kouddan <kouddan.yasser.work@gmail.com>
2024-10-20 16:11:38 +0100

    the best escape game ever

---
+++ routes/master/script.js
@@ -0,0 +1,80 @@
+// Import the utility functions
+const utils = require('./utils');
+
+// Handle Room Form Submissions
+document.getElementById('room1Form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const players = [
+        document.getElementById('room1Player1').value,
+        document.getElementById('room1Player2').value,
+        document.getElementById('room1Player3').value,
+        document.getElementById('room1Player4').value,
+        document.getElementById('room1Player5').value
+    ];
+    utils.changePlayersRoom1(players);
+    alert('Room 1 players updated successfully!');
+});
+
+document.getElementById('room2Form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const players = [
+        document.getElementById('room2Player1').value,
+        document.getElementById('room2Player2').value,
+        document.getElementById('room2Player3').value,
+        document.getElementById('room2Player4').value,
+        document.getElementById('room2Player5').value
+    ];
+    utils.changePlayersRoom2(players);
+    alert('Room 2 players updated successfully!');
+});
+
+document.getElementById('room3Form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const players = [
+        document.getElementById('room3Player1').value,
+        document.getElementById('room3Player2').value,
+        document.getElementById('room3Player3').value,
+        document.getElementById('room3Player4').value,
+        document.getElementById('room3Player5').value
+    ];
+    utils.changePlayersRoom3(players);
+    alert('Room 3 players updated successfully!');
+});
+
+document.getElementById('room4Form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const players = [
+        document.getElementById('room4Player1').value,
+        document.getElementById('room4Player2').value,
+        document.getElementById('room4Player3').value,
+        document.getElementById('room4Player4').value,
+        document.getElementById('room4Player5').value
+    ];
+    utils.changePlayersRoom4(players);
+    alert('Room 4 players updated successfully!');
+});
+
+document.getElementById('room5Form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const players = [
+        document.getElementById('room5Player1').value,
+        document.getElementById('room5Player2').value,
+        document.getElementById('room5Player3').value,
+        document.getElementById('room5Player4').value,
+        document.getElementById('room5Player5').value
+    ];
+    utils.changePlayersRoom5(players);
+    alert('Room 5 players updated successfully!');
+});
+
+// Handle Key Form Submission
+document.getElementById('keysForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const keys = [
+        document.getElementById('key1').value,
+        document.getElementById('key2').value,
+        document.getElementById('key3').value
+    ];
+    utils.changeKeys(keys);
+    alert('Keys updated successfully!');
+});
---
+++ utils.js
@@ -0,0 +1,72 @@
+// utils.js
+
+const jsonData = {
+    "room1": ["younes", "bader", "mehdi", "yasser", "hamza"],
+    "room2": ["player1", "player2", "player3", "player4", "player5"],
+    "room3": ["player1", "player2", "player3", "player4", "player5"],
+    "room4": ["player1", "player2", "player3", "player4", "player5"],
+    "room5": ["player1", "player2", "player3", "player4", "player5"],
+    "keys": ["nature", "homme", "harmonie"]
+};
+
+// Function to change players in a room
+function changePlayersRoom1(players) {
+    jsonData.room1 = players;
+}
+
+function changePlayersRoom2(players) {
+    jsonData.room2 = players;
+}
+
+function changePlayersRoom3(players) {
+    jsonData.room3 = players;
+}
+
+function changePlayersRoom4(players) {
+    jsonData.room4 = players;
+}
+
+function changePlayersRoom5(players) {
+    jsonData.room5 = players;
+}
+
+// Function to get players in a room
+function getPlayersRoom1() {
+    return jsonData.room1;
+}
+
+function getPlayersRoom2() {
+    return jsonData.room2;
+}
+
+function getPlayersRoom3() {
+    return jsonData.room3;
+}
+
+function getPlayersRoom4() {
+    return jsonData.room4;
+}
+
+function getPlayersRoom5() {
+    return jsonData.room5;
+}
+
+// Function to get keys
+function getKeys() {
+    return jsonData.keys;
+}
+
+// Export the functions for use in other files
+module.exports = {
+    changePlayersRoom1,
+    changePlayersRoom2,
+    changePlayersRoom3,
+    changePlayersRoom4,
+    changePlayersRoom5,
+    getPlayersRoom1,
+    getPlayersRoom2,
+    getPlayersRoom3,
+    getPlayersRoom4,
+    getPlayersRoom5,
+    getKeys
+};
