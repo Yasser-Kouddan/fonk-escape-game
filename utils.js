fc4db187012675a0e8ac2c3141deef87c362ac66
Yasser Kouddan <kouddan.yasser.work@gmail.com>
2024-11-16 13:59:09 +0100

    latest changes

--- utils.js
+++ utils.js
@@ -1,54 +1,13 @@
 // utils.js
 
 const jsonData = {
-    "room1": ["oumaima", "zyad", "badr eddine", "imane", "salma"],
-    "room2": ["player1", "player2", "player3", "player4", "player5"],
-    "room3": ["player1", "player2", "player3", "player4", "player5"],
-    "room4": ["oumaima", "zyad", "badr eddine", "imane", "salma"],
-    "room5": ["player1", "player2", "player3", "player4", "player5"],
+    "valeurs": ["Respect", "Engagement", "Apprentissage", "Travaille", "Honnetete"],
     "keys": ["nature", "homme", "harmonie"]
 };
 
 // Function to change players in a room
-function changePlayersRoom1(players) {
-    jsonData.room1 = players;
-}
-
-function changePlayersRoom2(players) {
-    jsonData.room2 = players;
-}
-
-function changePlayersRoom3(players) {
-    jsonData.room3 = players;
-}
-
-function changePlayersRoom4(players) {
-    jsonData.room4 = players;
-}
-
-function changePlayersRoom5(players) {
-    jsonData.room5 = players;
-}
-
-// Function to get players in a room
-function getPlayersRoom1() {
-    return jsonData.room1;
-}
-
-function getPlayersRoom2() {
-    return jsonData.room2;
-}
-
-function getPlayersRoom3() {
-    return jsonData.room3;
-}
-
-function getPlayersRoom4() {
-    return jsonData.room4;
-}
-
-function getPlayersRoom5() {
-    return jsonData.room5;
+function getValeurs() {
+    return jsonData.valeurs;
 }
 
 // Function to get keys
@@ -56,17 +15,5 @@ function getKeys() {
     return jsonData.keys;
 }
 
-// Export the functions for use in other files
-module.exports = {
-    changePlayersRoom1,
-    changePlayersRoom2,
-    changePlayersRoom3,
-    changePlayersRoom4,
-    changePlayersRoom5,
-    getPlayersRoom1,
-    getPlayersRoom2,
-    getPlayersRoom3,
-    getPlayersRoom4,
-    getPlayersRoom5,
-    getKeys
-};
+// Expose functions globally for browser use
+window.utils = { getKeys, getValeurs };
